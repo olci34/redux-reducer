faa805302f1e2d34a94e8075de2d94effd8edcfe
github username <github email address>
2021-05-25 00:41:24 -0400

    commit tree 225cfb5a702993c6d4c9787d3dbd414bc03e58fe

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]}
+        case 'REMOVE_FRIEND':
+            const index = state.friends.indexOf(state.friends.find(f => f.id === action.id))
+            const friends = [...state.friends]
+            friends.splice(index,1)
+            return {friends: friends}
+        default:
+            return state;
+    }
 }
